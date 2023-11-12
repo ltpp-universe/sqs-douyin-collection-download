f4431875c4a0ec25fa84cbe0bbe60c1679f53c31
wmzn-ltpp <1491579574@qq.com>
2023-11-12 18:18:58 +0800

    更新

--- utils/init.js
+++ utils/init.js
@@ -2,7 +2,7 @@
  * @Author: wmzn-ltpp 1491579574@qq.com
  * @Date: 2023-11-09 21:32:28
  * @LastEditors: wmzn-ltpp 1491579574@qq.com
- * @LastEditTime: 2023-11-12 17:44:54
+ * @LastEditTime: 2023-11-12 18:18:36
  * @FilePath: \sqs-douyin-collection-download\utils\init.js
  * @Description: Email:1491579574@qq.com
  * QQ:1491579574
@@ -12,7 +12,7 @@ const fs = require('fs');
 const path = require('path');
 const Console = require('sqs-console');
 const { sleep, show_error_exit_msg_time } = require('./base');
-const config_path = path.resolve(__dirname, '../') + '/DouYinDownloadConfig.json';
+const config_path = path.resolve(__dirname) + '/DouYinDownloadConfig.json';
 
 /**
  * 生成配置
