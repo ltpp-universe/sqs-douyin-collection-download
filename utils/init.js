c633a02b16ab09fb3ca40e34ea2e24dd37fff27c
wmzn-ltpp <1491579574@qq.com>
2023-11-12 18:52:12 +0800

    更新

--- utils/init.js
+++ utils/init.js
@@ -2,7 +2,7 @@
  * @Author: wmzn-ltpp 1491579574@qq.com
  * @Date: 2023-11-09 21:32:28
  * @LastEditors: wmzn-ltpp 1491579574@qq.com
- * @LastEditTime: 2023-11-12 18:42:51
+ * @LastEditTime: 2023-11-12 18:51:58
  * @FilePath: \sqs-douyin-collection-download\utils\init.js
  * @Description: Email:1491579574@qq.com
  * QQ:1491579574
@@ -22,7 +22,7 @@ const creatConfig = async function () {
         if (fs.existsSync(config_path)) {
             return Promise.resolve();
         }
-        const config_str = '{"db_host":"127.0.0.1","db_username":"","db_password":"","db_port":"","db_database":"","cookie":"","save_sql_path":"/DouYinDownload/video.db","save_progress_path":"/DouYinDownload/progress","save_path":"/DouYinDownload/video/","save_cursor_list_path":"/DouYinDownload/cursor_list","download":true,"updatedatabase":false,"update_list_length_limit":1000,"max_no_update_time_to_delete":30}';
+        const config_str = '{"db_host":"127.0.0.1","db_username":"","db_password":"","db_port":"3366","db_database":"","cookie":"","save_sql_path":"/video.db","save_progress_path":"/progress.cache","save_path":"/video.cache/","save_cursor_list_path":"/cursor_list.cache","download":true,"updatedatabase":false,"update_list_length_limit":1000,"max_no_update_time_to_delete":30}';
         fs.mkdirSync(path.dirname(config_path), { recursive: true });
         fs.writeFileSync(config_path, config_str);
         Console.log(0, `请填写配置文件（${config_path}）的必备字段后重新运行`);
