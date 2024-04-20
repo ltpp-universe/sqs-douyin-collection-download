b47e2e4b3ef180dff1a6b01334d7005930f642ff
尤雨东 <root@ltpp.vip>
2024-04-20 08:47:22 +0800

    feat:update shell

---
+++ sh/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git remote add ltpp ssh://git@git.ltpp.vip:66/root/sqs-douyin-collection-download.git;
+git remote add github git@github.com:wmzn-ltpp/sqs-douyin-collection-download.git;
---
+++ sh/push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git add .;
+echo -n "Please enter the update message and press Enter:"
+read update_message
+if [ -z "$update_message" ]; then
+    echo "The update message cannot be empty. Please rerun the script and provide a valid update message."
+    echo "Press Enter to exit...";
+    read -n 1;
+    exit 1
+fi
+git commit -m "feat:$update_message";
+git push github main;
+echo -e "\e[32mgithub push finish\e[0m";
+git push ltpp main;
+echo -e "\e[32mltpp push finish\e[0m";
+echo "Press Enter to continue...";
+read -n 1;
