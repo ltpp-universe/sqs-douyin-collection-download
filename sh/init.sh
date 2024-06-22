b8946c17401e60edacc69ce781751bb020f959af
尤雨东 <root@ltpp.vip>
2024-06-22 13:41:23 +0800

    feat:update

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git remote add ltpp ssh://git@git.ltpp.vip:66/root/sqs-douyin-collection-download.git;
 git remote add github git@github.com:ltpp-universe/sqs-douyin-collection-download.git;
+git remote add origin git@github.com:ltpp-universe/sqs-douyin-collection-download.git;
