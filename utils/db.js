b0e472ad831e190688ea364848a00e502054af25
尤雨东 <root@ltpp.vip>
2024-09-17 17:50:15 +0800

    feat:update

--- utils/db.js
+++ utils/db.js
@@ -6,43 +6,44 @@
  * @FilePath: \node\utils\db.js
  * @Description: Email:1491579574@qq.com
  * QQ:1491579574
- * Copyright (c) 2023 by SQS, All Rights Reserved. 
+ * Copyright (c) 2023 by SQS, All Rights Reserved.
  */
-const mysql = require("mysql")
-const Console = require('sqs-console');
+const mysql = require('mysql');
+const Console = require('node-colour-console');
 const { readConfig } = require('./init');
 let connection = null;
 
 const init = async function () {
-    return new Promise(async (resolve) => {
-        const { db_host, db_username, db_password, db_port, db_database } = await readConfig();
-        Console.log(0, '开始连接数据库');
-        connection = mysql.createConnection({
-            host: db_host,
-            user: db_username,
-            password: db_password,
-            port: db_port,
-            database: db_database,
-            debug: false,
-            multipleStatements: true,
-            charset: 'UTF8MB4_UNICODE_CI'
-        });
-        connection.connect();
-        connection.on('error', (err) => {
-            if (err) {
-                if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-                    init();
-                }
-            }
-        });
-        Console.log(0, '数据库连接成功');
-        resolve();
+  return new Promise(async (resolve) => {
+    const { db_host, db_username, db_password, db_port, db_database } =
+      await readConfig();
+    Console.log('开始连接数据库');
+    connection = mysql.createConnection({
+      host: db_host,
+      user: db_username,
+      password: db_password,
+      port: db_port,
+      database: db_database,
+      debug: false,
+      multipleStatements: true,
+      charset: 'UTF8MB4_UNICODE_CI',
     });
+    connection.connect();
+    connection.on('error', (err) => {
+      if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+          init();
+        }
+      }
+    });
+    Console.log('数据库连接成功');
+    resolve();
+  });
 };
 
 const disconnectDb = function () {
-    connection.end();
-    Console.log(0, '数据库断开连接成功');
+  connection.end();
+  Console.log('数据库断开连接成功');
 };
 
 /**
@@ -50,24 +51,24 @@ const disconnectDb = function () {
  * @param {String} sql
  */
 const runSql = function (sql) {
-    try {
-        return new Promise(async (resolve) => {
-            if (!connection) {
-                await init();
-            }
-            connection.query(sql, (err, result) => {
-                if (err) {
-                    Console.log(0, `SQL执行错误:${err}`, 'BgRed');
-                }
-                resolve(result);
-            });
-        });
-    } catch (err) {
-        Console.log(0, `数据库错误:${err}`, 'BgRed');
-        process.exit(1);
-    }
+  try {
+    return new Promise(async (resolve) => {
+      if (!connection) {
+        await init();
+      }
+      connection.query(sql, (err, result) => {
+        if (err) {
+          Console.log(`SQL执行错误:${err}`, 'BgRed');
+        }
+        resolve(result);
+      });
+    });
+  } catch (err) {
+    Console.log(`数据库错误:${err}`, 'BgRed');
+    process.exit(1);
+  }
 };
 
 module.exports = {
-    runSql
+  runSql,
 };
--- utils/init.js
+++ utils/init.js
@@ -6,54 +6,59 @@
  * @FilePath: \sqs-douyin-collection-download\utils\init.js
  * @Description: Email:1491579574@qq.com
  * QQ:1491579574
- * Copyright (c) 2023 by SQS, All Rights Reserved. 
+ * Copyright (c) 2023 by SQS, All Rights Reserved.
  */
 const fs = require('fs');
 const path = require('path');
-const Console = require('sqs-console');
-const { sleep, show_error_exit_msg_time, app_path, app_config_path } = require('./base');
+const Console = require('node-colour-console');
+const {
+  sleep,
+  show_error_exit_msg_time,
+  app_path,
+  app_config_path,
+} = require('./base');
 const config_path = app_path + app_config_path;
 
 /**
  * 生成配置
  */
 const creatConfig = async function () {
-    try {
-        if (fs.existsSync(config_path)) {
-            return Promise.resolve();
-        }
-        const config_str = '{"db_host":"127.0.0.1","db_username":"","db_password":"","db_port":3366,"db_database":"","cookie":"","save_sql_path":"/video.db","save_progress_path":"/progress","save_path":"/video/","save_cursor_list_path":"/cursor_list","download":true,"updatedatabase":false,"update_list_length_limit":1000,"max_no_update_time_to_delete":30}';
-        fs.mkdirSync(path.dirname(config_path), { recursive: true });
-        fs.writeFileSync(config_path, config_str);
-        Console.log(0, `请填写配置文件（${config_path}）的必备字段后重新运行`);
-    } catch (error) {
-        Console.log(0, `生成配置文件（${config_path}）出错:${error}`);
+  try {
+    if (fs.existsSync(config_path)) {
+      return Promise.resolve();
     }
-    await sleep(show_error_exit_msg_time);
-    process.exit(1);
-}
+    const config_str =
+      '{"db_host":"127.0.0.1","db_username":"","db_password":"","db_port":3366,"db_database":"","cookie":"","save_sql_path":"/video.db","save_progress_path":"/progress","save_path":"/video/","save_cursor_list_path":"/cursor_list","download":true,"updatedatabase":false,"update_list_length_limit":1000,"max_no_update_time_to_delete":30}';
+    fs.mkdirSync(path.dirname(config_path), { recursive: true });
+    fs.writeFileSync(config_path, config_str);
+    Console.log(`请填写配置文件（${config_path}）的必备字段后重新运行`);
+  } catch (error) {
+    Console.log(`生成配置文件（${config_path}）出错:${error}`);
+  }
+  await sleep(show_error_exit_msg_time);
+  process.exit(1);
+};
 
 /**
- * 读取配置 
+ * 读取配置
  * @returns {Object} res
  */
 const readConfig = async function () {
-    try {
-        await creatConfig();
-        if (!fs.existsSync(config_path)) {
-            Console.log(0, '配置文件不存在');
-            process.exit(1);
-        }
-        const config_str = fs.readFileSync(config_path);
-        const config_json = JSON.parse(config_str);
-        return config_json;
-    } catch (error) {
-        Console.log(0, `读取配置文件出错:${error}`);
-        process.exit(1);
+  try {
+    await creatConfig();
+    if (!fs.existsSync(config_path)) {
+      Console.log('配置文件不存在');
+      process.exit(1);
     }
-}
-
+    const config_str = fs.readFileSync(config_path);
+    const config_json = JSON.parse(config_str);
+    return config_json;
+  } catch (error) {
+    Console.log(`读取配置文件出错:${error}`);
+    process.exit(1);
+  }
+};
 
 module.exports = {
-    readConfig
-}
+  readConfig,
+};
