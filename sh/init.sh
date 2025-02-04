3fb4dbb47a00f84535343786558749ee8eb14956
尤雨东 <root@ltpp.vip>
2025-02-04 10:01:39 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/sqs-douyin-collection-download.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/sqs-douyin-collection-download.git;
 git remote add github git@github.com:ltpp-universe/sqs-douyin-collection-download.git;
 git remote add origin git@github.com:ltpp-universe/sqs-douyin-collection-download.git;
