330bd1501ab007f22f0db32b115f1e6749165da4
尤雨东 <root@ltpp.vip>
2025-02-05 21:26:09 +0800

    feat:

--- sh/push.sh
+++ sh/push.sh
@@ -13,7 +13,7 @@ git commit -m "feat:$update_message";
 git push github main;
 echo -e "\e[32mgithub push finish\e[0m";
 git push jihulab main;
-echo -e "\e[32mltpp push finish\e[0m";
+echo -e "\e[32mjihulab push finish\e[0m";
 git push origin main;
 echo -e "\e[32morigin push finish\e[0m";
 echo "Press Enter to continue...";
