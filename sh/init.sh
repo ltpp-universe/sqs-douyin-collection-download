6ab7c7ebb2e05211dbecc2d46a904dbc1a448b7b
尤雨东 <root@ltpp.vip>
2025-02-09 22:04:43 +0800

    n

--- sh/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git init;
-git config core.ignorecase false;
-git remote add gitee git@gitee.com:ltpp-universe/sqs-douyin-collection-download.git;
-git remote add origin git@github.com:ltpp-universe/sqs-douyin-collection-download.git;
